54e7845adda5f8f7731c3b5fd9d00c48ddeb7482
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-09-08 19:14:18 +0200

    Created add.html and add-funktionality.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 
@@ -6,12 +6,51 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Render the homepage with a list of blog posts loaded from the JSON file.
 
+    Returns:
+        Rendered HTML template with the list of blog posts.
+    """
     with open("data.json", "r", encoding="UTF-8") as fileobj:
-        #fileobj.read()
         blog_posts = json.load(fileobj)
 
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    """
+    Handle GET and POST requests to add a new blog post.
+
+    GET: Render the form to create a new blog post.
+    POST: Receive form data, append new post to JSON file, and redirect to homepage.
+
+    Returns:
+        Redirect or rendered HTML template depending on request method.
+    """
+    if request.method == 'POST':
+        title = request.form.get("title", "--")
+        author = request.form.get("author", "--")
+        content = request.form.get("content", "--")
+
+        with open("data.json", "r", encoding="UTF-8") as fileobj:
+            blog_posts = json.load(fileobj)
+
+        new_post = {
+            "id": len(blog_posts) + 1,
+            "author": author,
+            "title": title,
+            "content": content
+        }
+
+        blog_posts.append(new_post)
+
+        with open("data.json", "w", encoding="UTF-8") as fileobj:
+            json.dump(blog_posts, fileobj, indent=4, ensure_ascii=False)
+
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
